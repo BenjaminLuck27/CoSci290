4a67b9b3beca0e1014b2d93a4709ebfbeaa36351
bluck64 <bluck64@gmail.com>
2018-03-22 18:30:11 -0400

    Lab11

---
+++ Lab11.java
@@ -0,0 +1,37 @@
+/*
+Benjamin Luck 
+CoSci 290 
+
+*/
+
+import java.util.Scanner;
+
+  public class Lab11{
+  
+   public static void main(String[] args){
+  
+   Scanner input = new Scanner(System.in);
+   // User input on how many numbers should be printed  
+   System.out.println("Enter n:");
+   int n = input.nextInt();
+   // Displaying the matrix
+   printMatrix(n);
+   }
+    // Beginning of matrix 
+    public static void printMatrix(int n){
+    do{
+    for(int i = 0; i < n; i++){
+    int random = 0 + (int)(Math.random() * 2);
+    System.out.print(random);
+    }
+     
+      
+      
+        }
+  }
+  }   
+      
+    
+      
+    
+  
